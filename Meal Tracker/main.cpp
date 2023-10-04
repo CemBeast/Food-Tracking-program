9777a54f526b6d81bad7e1623b2bbe929a693f42
CemBeast <cem.beyenal@wsu.edu>
2023-10-03 23:22:17 -0700

    Initial Commit

---
+++ Meal Tracker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Meal Tracker
+//
+//  Created by Cem Beyenal on 10/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
